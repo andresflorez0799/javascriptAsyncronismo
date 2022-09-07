8f1fe348b9efbc16e145b356bd556bb4696fd3b2
andresflorez0799 <andresflorez0799@gmail.com>
2022-09-07 10:37:56 -0500

    se agrega ejemplo de uso de Promise

--- src/Promise/index.js
+++ src/Promise/index.js
@@ -1,3 +1,23 @@
 
+const promise = new Promise((resolve, reject) => {
+    resolve("todo ok, se completo el proceso");
+});
 
+const cows = 10;
 
+const countCows = new Promise((resolve, reject) => {
+
+    if(cows >= 15) {
+        resolve("Tenemos suficientes vacas aun en la granja!");
+    }else{
+        reject("No hay suficientes vacas");
+    }
+
+});
+
+countCows
+.then((resultado) => {
+    console.log(resultado);
+})
+.catch((error) => console.error(error))
+.finally(() => console.log("Listo, se ha terminado toda la peticion"));
