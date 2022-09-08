2a33e6745dba248cd2ce2bdc82d0f92e1f3963cc
andresflorez0799 <andresflorez0799@gmail.com>
2022-09-07 23:14:39 -0500

    ejemplo de generator en js

---
+++ src/generator/index.js
@@ -0,0 +1,25 @@
+
+function* gen(){
+    yield 1;
+    yield 2;
+    yield 3;
+}
+
+const g = gen();
+
+console.log(g.next().value);
+console.log(g.next().value);
+
+function* iterate(array){
+    for(let value of array){
+        yield value;
+    }
+}
+
+const it = iterate([1,2,3]);
+console.log(it.next());
+console.log(it.next());
+console.log(it.next());
+console.log(it.next());
+
+
