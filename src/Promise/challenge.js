d55e70b8d2bd1f700979176be5da4bfe9cb361cd
andresflorez0799 <andresflorez0799@gmail.com>
2022-09-07 19:33:33 -0500

    se agregan metodos de consulta con fecth

---
+++ src/Promise/challenge.js
@@ -0,0 +1,12 @@
+import fetch from 'node-fetch';
+
+const api = 'https://api.escuelajs.co/api/v1';
+
+fetch(`${api}/products`)
+.then(response => response.json())
+.then(products => fetch(`${api}/products/${products[0].id}`))
+.then(response => response.json())
+.then(product => fetch(`${api}/categories/${product.category.id}`))
+.then(response => response.json())
+.then(category => console.log(category))
+.catch(error => console.error(error));
